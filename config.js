00a77a5f8d2aeca9b35e8b640e68633fc20fa604
carmezy <130802247+carmezy@users.noreply.github.com>
2025-08-07 17:45:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-    ? 'localhost:3000'
+    ? 'https://pagina-de-seguridad.onrender.com'
     : process.env.LOCAL_URL;
 
   const MONGO_URI = process.env.NODE_ENV === 'production'
       ? process.env.MONGO_URI_PROD
       : process.env.MONGO_URI_TEST
+
 
  module.exports = {PAGE_URL, MONGO_URI} ;
